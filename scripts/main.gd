b15674b34f1119e475378331e13ae7251a6c39af
quareterra <thatcactusperson@gmail.com>
2026-01-29 17:51:32 +0200

    added food spawn

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,16 @@
 extends Node2D
-var appear_timer = 2.0
+var appear_timer = 0.0
+var time_to_appear = 2.0
 var food = load("res://scenes/food.tscn")
 
 func _process(delta: float) -> void:
 	appear_timer += delta
-	pass
+	if appear_timer > time_to_appear:
+		spawn_food()
+		appear_timer = 0.0
 	
+func spawn_food():
+	var current_food = food.instantiate()
+	current_food.global_position = Vector2(randf(), randf()) * 1000
+
+	add_child(current_food)
