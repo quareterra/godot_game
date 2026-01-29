b439f9a8a77d734d732a648edbca7da0125bd55c
quareterra <thatcactusperson@gmail.com>
2026-01-29 17:07:13 +0200

    added movement to mama

--- scripts/cell_mama.gd
+++ scripts/cell_mama.gd
@@ -1,6 +1,16 @@
 extends CharacterBody2D
+var speed = 2000
 
 func _process(delta: float) -> void:
+	velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
-		pass
-		
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed * delta
+	move_and_slide()
